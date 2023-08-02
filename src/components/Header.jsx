2873140e40a10004829b1e1edf31f35274b45947
Dukewendo <dommct@yahoo.co.uk>
2023-08-02 20:34:33 +0100

    barbers

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className='header-container'>
+      <h2 className='header-title'>Joe's Barber Shop</h2>
+      <ul className='header-menu'>
+        <li>About</li>
+        <li>Barbers</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  )
+}
