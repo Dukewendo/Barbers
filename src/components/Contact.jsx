798575be73117f586bc2989ae0eb84fc41d92d7e
Dukewendo <dommct@yahoo.co.uk>
2023-08-17 20:46:09 +0100

    contact page layout

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,12 +1,33 @@
 import React from "react";
 import Header from "./Header";
-import './contact.css';
+import "./contact.css";
+
+//images
+import phone from "../images/phone.png";
+import chat from "../images/speech-bubble.png";
 
 export default function Contact() {
   return (
     <div>
       <Header />
-      <p>Contact page</p>
+      <h3 className="page-title">Contact us</h3>
+
+      <div className="contact-container">
+        <div className="telephone-container">
+          <img className="mobile-pic" src={phone} alt="mobile-phone" />
+          <p>Call us on 0782354553</p>
+        </div>
+
+        <div className="chat-container">
+          <img className="chat-pic" src={chat} alt="chat-bubble" />
+          <p>live chat</p>
+        </div>
+        <div className="form-container">
+          <img className="chat-pic" src={chat} alt="chat-bubble" />
+          <p>live chat</p>
+        </div>
+
+      </div>
     </div>
   );
 }
