653cd4f6adb02b032ef1d05f236b37225d8cadca
Dukewendo <dommct@yahoo.co.uk>
2023-08-31 19:15:18 +0100

    contact grid and css

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -20,9 +20,24 @@ export default function Contact() {
         <button className="book-button">BOOK APPOINTMENT</button>
         <div className="contact-info">
           <h3 className="contact-header">CONTACT UNCLE B's</h3>
-          <img src={phone} className="mobile-pic" alt="phone" />
-          <img src={email} className="mail-pic" alt="email" />
+          <p className="small-address">113 House Street, Townsville</p>
+          <div className="phone-details-container">
+            <img src={phone} className="mobile-pic" alt="phone" />
+            <p className="mobile-number">(+44) 84728395</p>
+          </div>
 
+          <div className="email-details-container">
+            <img src={email} className="mail-pic" alt="email" />
+            <p className="email-address">hello@UncleBs.com</p>
+          </div>
+
+          <div className="opening-time-container">
+            <h3 className="opening-times">OPEN 7 DAYS A WEEK</h3>
+            <p>Mon-Thurs 10AM - 9PM</p>
+            <p>Fri 10AM - 7PM</p>
+            <p>Sat 8AM - 6PM</p>
+            <p>Sun 9AM - 5PM</p>
+          </div>
         </div>
         <p className="contact-info"></p>
       </div>
