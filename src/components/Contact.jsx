83dc13979fed6ffadf7b02168c43ef2919efb8f3
Dukewendo <dommct@yahoo.co.uk>
2023-09-03 16:38:31 +0100

    style contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,8 +5,8 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 
 //images
-import phone from "../images/phone.png";
-import email from "../images/email.png";
+import phone from "../images/small-phone.png";
+import email from "../images/small-email.png";
 
 export default function Contact() {
   return (
@@ -17,26 +17,32 @@ export default function Contact() {
       </h3>
 
       <div className="contact-container">
+        <div className="button-container">
         <button className="book-button">BOOK APPOINTMENT</button>
+        </div>
+        
         <div className="contact-info">
           <h3 className="contact-header">CONTACT UNCLE B's</h3>
           <p className="small-address">113 House Street, Townsville</p>
           <div className="phone-details-container">
             <img src={phone} className="mobile-pic" alt="phone" />
-            <p className="mobile-number">(+44) 84728395</p>
+            <a href ="tel:+4484728395" className="mobile-number">(+44) 84728395</a>
           </div>
 
           <div className="email-details-container">
             <img src={email} className="mail-pic" alt="email" />
-            <p className="email-address">hello@UncleBs.com</p>
+            <a href= "mailto:hello@UncleBs.com" className="email-address">hello@UncleBs.com</a>
           </div>
 
           <div className="opening-time-container">
-            <h3 className="opening-times">OPEN 7 DAYS A WEEK</h3>
+            <h3 className="opening-title">OPEN 7 DAYS A WEEK</h3>
+            <div className="weekday-times">
             <p>Mon-Thurs 10AM - 9PM</p>
             <p>Fri 10AM - 7PM</p>
             <p>Sat 8AM - 6PM</p>
             <p>Sun 9AM - 5PM</p>
+            </div>
+            
           </div>
         </div>
       </div>
