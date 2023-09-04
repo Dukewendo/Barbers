9aa2d2627971e27cedd6626968e7857771c0cad2
Dukewendo <dommct@yahoo.co.uk>
2023-09-04 22:05:00 +0100

    styling google map iframe

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -17,32 +17,48 @@ export default function Contact() {
       </h3>
 
       <div className="contact-container">
+        <iframe
+          className="main-map"
+          title="map"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7191.31798668985!2d-74.00955153185734!3d40.72733971295217!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2suk!4v1693859366799!5m2!1sen!2suk"
+          width="600"
+          height="450"
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+        ></iframe>
+
         <div className="button-container">
-        <button className="book-button">BOOK APPOINTMENT</button>
+          <button className="book-button">
+            <span>BOOK APPOINTMENT</span>
+          </button>
         </div>
-        
+
         <div className="contact-info">
           <h3 className="contact-header">CONTACT UNCLE B's</h3>
-          <p className="small-address">113 House Street, Townsville</p>
+          <p className="small-address">113 Flushing Ave, New York</p>
           <div className="phone-details-container">
             <img src={phone} className="mobile-pic" alt="phone" />
-            <a href ="tel:+4484728395" className="mobile-number">(+44) 84728395</a>
+            <a href="tel:+4484728395" className="mobile-number">
+              (+44) 84728395
+            </a>
           </div>
 
           <div className="email-details-container">
             <img src={email} className="mail-pic" alt="email" />
-            <a href= "mailto:hello@UncleBs.com" className="email-address">hello@UncleBs.com</a>
+            <a href="mailto:hello@UncleBs.com" className="email-address">
+              hello@UncleBs.com
+            </a>
           </div>
 
           <div className="opening-time-container">
             <h3 className="opening-title">OPEN 7 DAYS A WEEK</h3>
             <div className="weekday-times">
-            <p>Mon-Thurs 10AM - 9PM</p>
-            <p>Fri 10AM - 7PM</p>
-            <p>Sat 8AM - 6PM</p>
-            <p>Sun 9AM - 5PM</p>
+              <p>Mon-Thurs 10AM - 9PM</p>
+              <p>Fri 10AM - 7PM</p>
+              <p>Sat 8AM - 6PM</p>
+              <p>Sun 9AM - 5PM</p>
             </div>
-            
           </div>
         </div>
       </div>
