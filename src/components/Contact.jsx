0a5b43feca56fb56a3754138cff5dd3013c43eb3
Dukewendo <dommct@yahoo.co.uk>
2023-09-09 16:08:55 +0100

    media queries for mobile

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -23,18 +23,19 @@ export default function Contact() {
       <div className="contact-page-container">
         <div className="map-frame">
           <iframe
+          className="iframe-map"
             title="map"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7191.31798668985!2d-74.00955153185734!3d40.72733971295217!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2suk!4v1693859366799!5m2!1sen!2suk"
-            width="600"
-            height="450"
             allowfullscreen=""
             loading="lazy"
             referrerpolicy="no-referrer-when-downgrade"
           ></iframe>
         </div>
 
-        <div className="contact-container"></div>
+        <div className="contact-container">
         <div className="contact-info">
+        </div>
+        
           <div className="button-container">
             <button className="book-button">
               <span>BOOK APPOINTMENT</span>
